72a09fa4c107559e51e271bd5f938b768622606e
yangyuwei <627888464@qq.com>
2019-10-13 01:51:42 +0800

    微信小程序案例base2事件对象/监听/冒泡捕获/数据处理/生命周期

--- pages/event/index.js
+++ pages/event/index.js
@@ -5,62 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 1. 初始一个页面中的数据
+    msg: '我是小程序中初始的数据'
+  },
+// 2. 定义事件回调函数(小程序中事件的回调无需methods而是直接定义！！)
+  // 给回调函数传参可以获得事件对象event/e/ev/自定名
+  clicked: function (ev) {
+    console.log('点击了...');
 
+/*
+console.log(ev);// 事件对象e/ev
+
+console.log(ev.target.dataset.role);通过ev.target可以获得wxml中的自定义属性
+否则js 无法与 wxml 通信！！！
+*/
+   
+// this.msg = '改变后的数据！！！';// 语法错误 undefind
+console.log(this.data.msg);// 获取初始数据
+this.data.msg = '改变后的数据！！！'; //无法更改初始数据
+  
+/*
+要改变小程序中的初始数据，只能通过 setData 这个方法
+setData 是实现数据绑定的重要方法, 
+setData 是小程序提供方法，接受一个对象做为参数
+*/
+this.setData({
+  msg: '改变后的数据！！！'
+})
+    
+ 
+  },
+  in:function () {
+    console.log('获得了焦点...');
+  },
+  out: function () {
+    console.log('失去了焦点...');
+  },
+  parent: function () {
+    console.log('父盒子点击了...')
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  child: function () {
+    console.log('子盒子点击了...')
   },
+  // 3. onLoad是小程序的生命周期 类似vue中的created
+  // 当页面加载完毕时触发执行的回调函数 自动执行
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  onLoad: function () {
+    console.log('页面加载了...');
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 页面一刷新会自动在 页面(自动)加载时触发 渲染页面
+    this.setData({
+      msg:'页面加载时，改变了数据...'
+    })
   }
 })
