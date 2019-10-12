d88defdb1929c56f841c7fad7b5038ec103b8871
yangyuwei <627888464@qq.com>
2019-10-13 00:06:02 +0800

    微信小程序案例base2数据渲染wx:if/else/for/key/index/item {{插值表达式}} hidden

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,18 +4,24 @@ Page({
     a:5,
     b: 6,
     list: [
-      152,
-      133,
-      165,
-      158
+      '你',
+      '我',
+      '他',
+      '她'
     ],
     user: {
       name: 'yyw',
       age: 18,
       hight: 160
     },
-    students:{
-      
-    }
+    students: [
+      {name: '小明', age: 18, gender: '男'},
+      {name: '小花', age: 17, gender: '女'},
+      {name: '小李', age: 16, gender: '女'},
+      {name: '小丽', age: 19, gender: '男'},
+      {name: '小刚', age: 18, gender: '男'},
+      {name: '小红', age: 16, gender: '女'},
+      {name: '小白', age: 18, gender: '男'}
+    ]
   }
 })
